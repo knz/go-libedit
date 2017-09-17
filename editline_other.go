e458ce5cfcc93c5753257e10c1836b9f80dcc9ac
Raphael 'kena' Poss <knz@thaumogen.net>
2017-09-17 14:31:34 -0400

    Provide non-unix compatibility.

---
+++ editline_other.go
@@ -0,0 +1,66 @@
+// +build !darwin,!freebsd,!linux,!openbsd,!netbsd,!dragonfly
+
+package libedit
+
+import (
+	"bufio"
+	"os"
+)
+
+type state struct {
+	reader                *bufio.Reader
+	promptGenLeft         LeftPromptGenerator
+	line                  string
+	stdin, stdout, stderr *os.File
+}
+
+var editors []state
+
+func InitFiles(stdin, stdout, stderr *os.File) (EditLine, error) {
+	st := state{
+		reader: bufio.NewReader(stdin),
+		stdin:  stdin, stdout: stdout, stderr: stderr,
+	}
+	editors = append(editors, st)
+	return EditLine(len(editors) - 1), nil
+}
+
+func (el EditLine) Close()                                {}
+func (el EditLine) SaveHistory() error                    { return nil }
+func (el EditLine) AddHistory(_ string) error             { return nil }
+func (el EditLine) LoadHistory(_ string) error            { return nil }
+func (el EditLine) SetAutoSaveHistory(_ string, _ bool)   {}
+func (el EditLine) UseHistory(_ int, _ bool) error        { return nil }
+func (el EditLine) SetCompleter(_ CompletionGenerator)    {}
+func (el EditLine) SetLeftPrompt(_ LeftPromptGenerator)   {}
+func (el EditLine) SetRightPrompt(_ RightPromptGenerator) {}
+
+func (el EditLine) Stdin() *os.File {
+	return editors[el].stdin
+}
+
+func (el EditLine) Stdout() *os.File {
+	return editors[el].stdout
+}
+
+func (el EditLine) Stderr() *os.File {
+	return editors[el].stderr
+}
+
+func (el EditLine) GetLineInfo() (string, int) {
+	return el.line, len(el.line)
+}
+
+func (el EditLine) GetLine() (string, error) {
+	st := &editors[el]
+	if st.promptGenLeft != nil {
+		st.stdout.WriteString(st.promptGenLeft.GetLeftPrompt())
+		st.stdout.Sync()
+	}
+	line, err := st.reader.ReadString('\n')
+	if err != nil {
+		return "", err
+	}
+	st.line = line
+	return st.line, nil
+}

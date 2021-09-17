a159e858c7909cff47993176e0092a3a9579c7cc
David Pacheco <dap@oxidecomputer.com>
2021-09-16 20:41:54 -0700

    add illumos support

--- editline_other.go
+++ editline_other.go
@@ -12,7 +12,7 @@
 // implied. See the License for the specific language governing
 // permissions and limitations under the License.
 
-// +build !darwin,!freebsd,!linux,!openbsd,!netbsd,!dragonfly
+// +build !darwin,!freebsd,!illumos,!linux,!openbsd,!netbsd,!dragonfly
 
 package libedit
 
